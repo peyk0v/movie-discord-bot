564b743aa5e0ac64fee52254e1347325c0c089fc
peykov <peykovn@gmail.com>
2022-01-15 18:32:05 -0300

    fix message matching

--- attachMovieData.js
+++ attachMovieData.js
@@ -6,24 +6,19 @@ const ATTACHED_FILE_MESSAGE = /\*\*\*.*\*\*\*/im;
 
 async function readTextFromAttachedFile(channel) {
   const attachedMessages = await filterAttachedMessages(channel)
-  
   if(attachedMessages.length === 0) {
     throw new AttachedNotFoundException()
   }
-
   const movieUrl = getMovieUrl(attachedMessages)
-
   if(!movieUrl) {
     throw new MovieFileAttachException()
   };
-
   return await getFileText(movieUrl)
 }
 
 async function filterAttachedMessages(channel) {
   let attachedMessages = []
   const _messages = await channel.messages.fetch({limit: 50})
-  
   if(_messages.size > 0) {
     _messages.forEach(_message => {
       if(_message.attachments.size > 0) {
@@ -31,17 +26,16 @@ async function filterAttachedMessages(channel) {
       }
     })
   }
-
   return attachedMessages
 }
 
 function getMovieUrl(attachedMessages) {
-  return attachedMessages.forEach(message => {
-    if(ATTACHED_FILE_MESSAGE.test(message.content)){
-      //SI LLEGA A PASAR ALGO, POSIBLEMENTE SEA ACA
-      return message.attachments.first()?.url;  
+  for(let message of attachedMessages) {
+    if(ATTACHED_FILE_MESSAGE.test(message.content)) {
+      console.log('ATTACHED FILE MATCHED')
+      return message.attachments.first()?.url
     }
-  })
+  }
 }
 
 async function getFileText(file) {
