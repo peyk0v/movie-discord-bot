fc9f4703540b54d1e66b02b93bed4de19e37c228
peykov <peykovn@gmail.com>
2022-01-17 00:32:00 -0300

    feat: add schema & exception handler

---
+++ db/schemas/movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const movie_schema = new mongoose.Schema({
+  movie_id: Number,
+  server_id: String,
+  seen_date: Date,
+  title: String,
+  release_date: String,
+  main_director: { name: String, image: String },
+  image_url: String,
+  directors: [String],
+  genres: [String],
+  vote_average: Number,
+  line_text: String,
+  line_number: Number
+});
+
+const Movie = mongoose.model('Movie', movie_schema)
+
+module.exports = Movie
