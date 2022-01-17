5ac3b5b3f594368dca6f7543b613edd930ce881f
peykov <peykovn@gmail.com>
2022-01-17 00:31:24 -0300

    refactor: messages on ds channel

---
+++ messages/attachFile.js
@@ -0,0 +1,126 @@
+const node_fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const { AttachedNotFoundException, MovieFileAttachException } = require('../exceptions/fileException')
+const { FetchMovieFileException, FetchEmptyFileException } = require('../exceptions/fetchDataException')
+const { overwritePreviousFile } = require('../movie_file/file');
+const { sendFinalMsg, addSuccessMessage } = require('./messages');
+const { ACTION } = require('../utils')
+
+const ATTACHED_FILE_MESSAGE = /\*\*\*.*\*\*\*/im;
+
+async function addMovieToAttachedFile(moviesObj, message) {
+  try {
+    //moviesObj <-- todas las pelis y la nueva
+    sendFinalMsg(message, movieData.new_movie)
+  } catch (error) {
+    throw error
+  }
+}
+
+/*
+async function readTextFromAttachedFile(channel) {
+  const attachedMessages = await filterAttachedMessages(channel)
+  if(attachedMessages.length === 0) {
+    throw new AttachedNotFoundException()
+  }
+  const movieUrl = getAttachFileURL(attachedMessages)
+  if(!movieUrl) {
+    throw new MovieFileAttachException()
+  };
+  const text = await getFileText(movieUrl)
+  if(text) {
+    return text
+  } else {
+    throw new FetchEmptyFileException()
+  }
+}*/
+
+function getAttachFileURL(attachedMessages) {
+  for(let message of attachedMessages) {
+    if(ATTACHED_FILE_MESSAGE.test(message.content)) {
+      return message.attachments.first()?.url
+    }
+  }
+}
+
+async function movieFileAlreadyExits(channel) {
+  const attachedMessages = await filterAttachedMessages(channel)
+  for(message of attachedMessages) {
+    if(ATTACHED_FILE_MESSAGE.test(message.content)) {
+      return true
+    }
+  }
+  return false
+}
+
+async function getFileText(file) {
+  try {
+    const response = await node_fetch(file);
+    if (!response.ok) {
+      throw new FetchMovieFileException()
+    }
+    return await response.text();
+  } catch (error) {
+    return error;
+  }
+}
+
+async function updateAttachMsg(msg, movieData) {
+  try {
+    const previousMsg = await findPreviousAttach(msg)
+    await previousMsg.delete()
+    await sendFinalMsg(msg, movieData)
+    addSuccessMessage(msg, movieData, ACTION.ADD)
+  } catch(e) {
+    throw e
+  }
+}
+/*
+async function updateAttachMsg(msg, movieData) {
+  try {
+    const previousMsg = await findPreviousAttach(msg)
+    await previousMsg.delete()
+      .then(() => {
+        sendFinalMsg(msg, movieData)
+          .then(() => {
+            addSuccessMessage(msg, movieData, ACTION.ADD)
+          })
+      })
+  } catch(e) {
+    throw e
+  }
+}*/
+
+async function findPreviousAttach(msg) {
+  const attachedMessages = await filterAttachedMessages(msg.channel)
+  if(attachedMessages.length === 0) {
+    throw new AttachedNotFoundException()
+  }
+  const lastMovieAttach = getLastMovieAttach(attachedMessages)
+  if(!lastMovieAttach) {
+    throw new MovieFileAttachException()
+  };
+  return lastMovieAttach
+}
+
+async function filterAttachedMessages(channel) {
+  let attachedMessages = []
+  const _messages = await channel.messages.fetch({limit: 50})
+  if(_messages.size > 0) {
+    _messages.forEach(_message => {
+      if(_message.attachments.size > 0) {
+        attachedMessages.push(_message)
+      }
+    })
+  }
+  return attachedMessages
+}
+
+function getLastMovieAttach(attachedMessages) {
+  for(let message of attachedMessages) {
+    if(ATTACHED_FILE_MESSAGE.test(message.content)) {
+      return message
+    }
+  }
+}
+
+module.exports = { addMovieToAttachedFile, movieFileAlreadyExits, updateAttachMsg }
---
+++ messages/messages.js
@@ -0,0 +1,72 @@
+const { MessageEmbed } = require('discord.js')
+const { ACTION } = require('../utils')
+
+function addSuccessMessage(msg, { title }, action) {
+  const embed = { color: 0x548f6f }
+  if(action === ACTION.ADD) {
+    embed.description = `Se agregó \_\_${title}\_\_ correctamente`
+  } else if (action === ACTION.EDIT) {
+    embed.description = `Nueva película \_\_${title}\_\_ se editó correctamente`
+  } else if (action === ACTION.DELETE) {
+    embed.description = `Se borró \_\_${title}\_\_ correctamente`
+  } else if (action === ACTION.CREATE_BASE) {
+    embed.description = `Archivo base creado correctamente`
+  }
+  msg.channel.send({embeds: [embed]}).then( _msg => {
+    msg.delete()
+    setTimeout(() => {_msg.delete()}, 10000)
+  })
+}
+
+function addFailureMessage(msg, text) {
+  const embed = {
+    color: 0xff0000,
+    description: 'Error: ' + text
+  }
+  msg.channel.send({embeds: [embed]}).then( _msg => {
+    msg.delete()
+    setTimeout(() => {_msg.delete()}, 10000)
+  })
+}
+
+function createTemporaryMessage(msg, text, time) {
+  msg.channel.send(text)
+    .then(_msg => {
+      setTimeout(() => { _msg.delete() }, time)
+    })
+}
+
+function createEmbed(msg, movieData) {
+  const embed = new MessageEmbed()
+	  .setColor('#548f6f')
+	  .setTitle(movieData.title)
+	  .setAuthor({ name: movieData.main_director.name, iconURL: movieData.main_director.image, url:  movieData.main_director.image})
+	  .setDescription('última peli vista')
+	  .setThumbnail(movieData.image_url)
+	  .addFields(
+      { name: '\u200B', value: '\u200B' },
+	  	{ name: 'Genres', value: movieData.genres.join(', ') },
+	  	{ name: 'director/s', value: movieData.directors.join(', '), inline: true },
+	  	{ name: 'release date', value: movieData.release_date, inline: true },
+      { name: 'vote average', value: movieData.vote_average, inline: true }
+	  )
+	  .addField('Inline field title', 'Some value here', true)
+	  .setImage(movieData.image_url)
+	  .setTimestamp()
+	  .setFooter({ text: msg.member.displayName, iconURL: msg.author.avatarURL() });
+
+  return embed
+}
+
+function sendFinalMsg(message, movieData) {
+  const embed = createEmbed(message, movieData);
+  const text = '\*\*\*movie\*\*\*'
+  return message.channel.send({content: text, embeds: [embed], files: ["movie_file/movies.txt"] });
+}
+
+module.exports = {
+  addSuccessMessage,
+  addFailureMessage,
+  createTemporaryMessage,
+  sendFinalMsg
+}
